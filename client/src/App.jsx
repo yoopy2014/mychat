fb3fcf1079c32292f33785f74654b5f1b0a9bec8
yoopy2014 <yoopy2014@gmail.com>
2026-08-11 12:03:43 +0900

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from 'react';
 import { io } from 'socket.io-client';
 import './App.css';
 
-const socket = io('http://localhost:3001', {
+const socket = io('https://chat-server-6t4i.onrender.com', {
   autoConnect: false,
 });
 
